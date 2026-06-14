651ffceb0c70b95f17adf452fdc505088dbedcb7
besnovatyj <metr31@yandex.ru>
2026-06-15 02:49:26 +0500

    to new modman

--- src/Module.php
+++ src/Module.php
@@ -1,6 +1,5 @@
 <?php
 
-
 /*
  * Copyright (c) 2026 Besnovatyj. Licensed under the MIT License.
  */
@@ -9,53 +8,27 @@ declare(strict_types=1);
 
 namespace Besnovatyj\ClearManager;
 
+use common\components\module\CmsModule;
+use modules\modmanNew\contract\DeclaresModule;
+use modules\modmanNew\contract\ProvidesAdminMenu;
+
 /**
  * Модуль управления и очистки временных данных
  *
  * Предоставляет централизованный функционал для сбора информации
  * о временных данных из различных модулей приложения и их очистки.
  */
-class Module extends \common\components\module\BaseModule
+class Module extends CmsModule implements
+    DeclaresModule, ProvidesAdminMenu
 {
     public const bool EDITABLE = true;
+    public const string VERSION = '1.0.0';
+    public const string MODULE_ID = 'ClearManager';
 
-    /**
-     * Возвращает конфигурацию меню для админ-панели
-     *
-     * @return array
-     */
-    public static function getAdminMenu(): array
-    {
-        return require __DIR__ . '/config/adminMenu.php';
-    }
+    public static function moduleId(): string { return self::MODULE_ID; }
+    public static function moduleVersion(): string { return self::VERSION; }
+    public static function isEditable(): bool { return self::EDITABLE; }
+    public static function adminMenu(): array { return require __DIR__.'/config/adminMenu.php'; }
+    public static function moduleConfig(): array { return require __DIR__.'/config/config.php'; }
 
-    /**
-     * Возвращает основную конфигурацию модуля
-     *
-     * @return array
-     */
-    public static function getConfig(): array
-    {
-        return require __DIR__ . '/config/config.php';
-    }
-
-    /**
-     * Возвращает настраиваемые опции модуля
-     *
-     * @return array
-     */
-    public static function getOptions(): array
-    {
-        return require __DIR__ . '/config/options.php';
-    }
-
-    /**
-     * Возвращает зависимости модуля
-     *
-     * @return array
-     */
-    public static function getDependencies(): array
-    {
-        return require __DIR__ . '/config/dependencies.php';
-    }
 }
